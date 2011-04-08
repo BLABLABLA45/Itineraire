824d62135d9c744c21fb64fa5d9911acfcd3786b
Geoffrey Roussat <dgeo_is_back@hotmail.com>
2011-04-08 11:33:05 +0200

    modif

--- ItineraireMetro/src/itinerairemetro/Main.java
+++ ItineraireMetro/src/itinerairemetro/Main.java
@@ -16,6 +16,7 @@ public class Main {
      */
     public static void main(String[] args) {
         System.out.println("COUCOsdsddddddddddddU");
+           System.out.println("sdsds");
     }
 
 }
