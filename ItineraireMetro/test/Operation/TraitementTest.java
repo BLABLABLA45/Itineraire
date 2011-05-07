24f964f4628dc52048f547172abf769dccb9f186
Geoffrey Roussat <dgeo_is_back@hotmail.com>
2011-05-07 13:33:04 +0200

    Ajout des nouvelles methodes

--- ItineraireMetro/test/Operation/TraitementTest.java
+++ ItineraireMetro/test/Operation/TraitementTest.java
@@ -87,7 +87,49 @@ public class TraitementTest {
         assertTrue(carte.getEnsTroncon().get(id).getStatut().equals(st));
     }
 
- 
+    @Test
+    public void testIncidentLigne(){
+        System.out.println("incidentLigne");
+        Ligne result = carte.getEnsLignes().get(1);
+        result.getListTron().get(1).setStatut("incident");
+        assertTrue(Traitement.incidentLigne(result));
+    }
+
+    @Test
+    public void testLigneCommune(){
+        System.out.println("ligneCommune");
+        String st = "ligne 1";
+        //carte.getEnsLignes().get(0).getListTron().get(0).setStatut("incident");
+        Ligne result = Traitement.ligneCommune(carte.getEnsStation().get(3), carte.getEnsStation().get(6));
+        assertTrue(result.getNom().equals(st));
+    }
+
+    @Test
+    public void testRechercheTroncon(){
+        System.out.println("rechercheTroncon");
+        String ligne = "ligne 1";
+        int id = 5;
+        Troncon result = Traitement.rechercheTroncon(ligne, id);
+        assertTrue(result.getNum() == 4);
+    }
+
+    @Test
+    public void testChangementLigne(){
+        System.out.println("changementLigne");
+        int dep = 0;
+        int arr = 7;
+        Station result = Traitement.changementLigne(carte.getEnsStation().get(dep), carte.getEnsStation().get(arr));
+        assertTrue(result.getNum() == 3);
+    }
+
+    @Test
+    public void testSensParcourt(){
+        System.out.println("sensParcourt");
+        int dep = 6;
+        int arr = 7;
+        int result = Traitement.sensParcourt(carte.getEnsStation().get(dep), carte.getEnsStation().get(arr));
+        assertEquals(result,2);
+    }
   
 
     @Test
@@ -96,11 +138,12 @@ public class TraitementTest {
         int dep = 7;
         int arr = 3;
 
-        Itineraire result = Traitement.rechercheItineraire(carte.getEnsStation().get(dep), carte.getEnsStation().get(arr));
-        //System.out.println("Entre la station "+ (dep+1) + " et la station " + (arr+1)+" =>" + result.getItineraire().getLast().getNum());
+        Itineraire result = Traitement.rechercheItineraire(carte.getEnsStation().get(dep),
+                carte.getEnsStation().get(arr));
+        //System.out.println("Entre la station "+ (dep+1) + " et la station " +
+        //(arr+1)+" =>" + result.getItineraire().getLast().getNum());
         //result.parcourtFinal();
-        assertTrue(result.getItineraire().getLast().getNum() == arr);
-
+        assertTrue(result.getItineraire().getLast().getNum() == (arr+1));
     }
 
 }
